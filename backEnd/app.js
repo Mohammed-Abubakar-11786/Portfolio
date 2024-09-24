af2d344c33966c3c4da4c4826367b43922ff7a9d
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-09-25 00:33:48 +0530

    handled cores3

--- backEnd/app.js
+++ backEnd/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "https://abubakar11786-portfolio.vercel.app" || "*",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
