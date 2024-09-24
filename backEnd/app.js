1674de3b5236714ac4585d71e0cd1d358f34f359
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-09-25 00:40:47 +0530

    handled cores5

--- backEnd/app.js
+++ backEnd/app.js
@@ -15,14 +15,14 @@ app.use(
   })
 );
 
-app.options("/submitContactForm", cors()); // Handle preflight requests
+// app.options("/submitContactForm", cors()); // Handle preflight requests
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 const PORT = process.env.PORT || 8181;
 
