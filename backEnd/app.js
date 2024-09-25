5503c0207c517e5351be662494c4834c7a017e5c
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-09-25 13:17:14 +0530

    handled cores25

--- backEnd/app.js
+++ backEnd/app.js
@@ -18,7 +18,13 @@ app.use(
 );
 
 // Handle preflight requests globally
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Additional middleware
 app.use(express.json());
